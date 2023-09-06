af78081ac1420dd1e17c24c63f6f4c4573d93f4d
M.Zareei <m.zareei@jabama.com>
2023-09-06 14:54:03 +0330

    commit

--- src/main/kotlin/org/example/springboot/App.kt
+++ src/main/kotlin/org/example/springboot/App.kt
@@ -9,4 +9,8 @@ class App
 fun main(args: Array<String>) {
     runApplication<App>(*args)
 }
-
+//TODO https://roadmap.sh/backend
+//TODO use clean architecture
+//TODO learn relational database
+//TODO learn relational no sql
+//TODO learn authentication
