beeeb06bd1d3cd34852ba13c3cd5a8ce6af62d6a
M.Zareei <m.zareei@jabama.com>
2023-09-06 14:30:31 +0330

    commit

--- src/main/kotlin/org/example/springboot/repository/MessageRepository.kt
+++ src/main/kotlin/org/example/springboot/repository/MessageRepository.kt
@@ -3,5 +3,9 @@ package org.example.springboot.repository
 import org.example.springboot.dto.Message
 import org.springframework.data.repository.CrudRepository
 
+//CrudRepository mainly provides CRUD functions.
+//PagingAndSortingRepository provides methods to do pagination and sorting records.
+//JpaRepository provides some JPA-related methods such as flushing the persistence context and deleting records in a batch.
+
 // this will implement all fun find, save, delete and... automatically
 interface MessageRepository : CrudRepository<Message, String>
